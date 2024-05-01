0e0408981956c1e854b1c33d3bba64d070a02b26
Larry Arrington <larryarrington@pursuit.org>
2024-05-01 15:24:12 -0400

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in HTML, Javascript, and CSS these days.
+      I've made a couple of programs in that language. I also like ReactJs and Figma. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/LarryPursuit" target="_blank">Github</a>!`,
+  "Vision Board": `This is my Vision Board . I hung it on the wall because I'm proud of it. I hope to one day develop the skills and Knowledge I need to start working as a Software Developer.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Anime. 
+    I also play alot of games and sometime stream for my friends. Go watch if you want a laugh and see how much I suck to <a href="https://www.twitch.tv/the25thwanderer" target="_blank">my channel</a>! (If you like the content)
+    You'll might just see a game you're currently playing through!`,
+  tv: `That's my TV. I've been watching Anime a lot, I recently like :
+     <a href="https://myanimelist.net/anime/21/One_Piece" target="_blank">One Piece</a>, <a href="https://myanimelist.net/anime/53580/Tensei_shitara_Slime_Datta_Ken_3rd_Season" target="_blank">That Time I Got Reincarnated as a Slime </a>,
+    <a href="https://myanimelist.net/anime/54900/Wind_Breaker" target="_blank">Wind Breaker</a> (sometimes) and <a href="https://myanimelist.net/anime/53407/Bartender__Kami_no_Glass" target="_blank">Bartender</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/LarryPursuit/Resume-CV/blob/main/LARRY%20ARRINGTON(7)%20-%20Software%20Developer.pdf" target="_blank">Check it out?</a>
+    Contact me at larryarrington@pursuit.org if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by reading some of Documentation <a href="https://kaboomjs.com/" target="_blank">here</a>.`,
+  library: `There are a lot of manga, webtoons, and programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Larry Arrington's portfolio, just close the tab.`,
+};
